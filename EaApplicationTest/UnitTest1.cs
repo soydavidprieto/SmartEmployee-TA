9ac6a1dad6e763275300c9b4847262c8354990e7
David Prieto Chavarro <dprie@softserveinc.com>
2023-04-12 16:01:14 -0500

    DriverFixture creation

--- EaApplicationTest/UnitTest1.cs
+++ EaApplicationTest/UnitTest1.cs
@@ -1,3 +1,8 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
 namespace EaApplicationTest
 {
     public class UnitTest1
@@ -5,6 +10,12 @@ namespace EaApplicationTest
         [Fact]
         public void Test1()
         {
+            var driver = new EdgeDriver();
+            driver.Navigate().GoToUrl("https://localhost:44322/");
+
+            driver.FindElement(By.Id("exampleInputEmail")).SendKeys("soydavidprieto@gmail.com");
+            driver.FindElement(By.Id("exampleInputPassword")).SendKeys("123456");
+            driver.FindElement(By.Id("btnLogin")).Click();
 
         }
     }
---
+++ EaFramework/Driver/DriverFixture.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Safari;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EaFramework.Driver
+{
+    public class DriverFixture
+    {
+        public IWebDriver Driver { get; }
+
+        public DriverFixture(BrowserType browserType)
+        {
+            Driver = GetWebDriver(browserType);
+            Driver.Navigate().GoToUrl("https://localhost:44322/");
+        }
+
+        private IWebDriver GetWebDriver(BrowserType browserType)
+        {
+            return browserType switch
+            {
+                BrowserType.Chrome => new ChromeDriver(),
+                BrowserType.Safari => new SafariDriver(),
+                BrowserType.Firefox => new FirefoxDriver(),
+                _ => new ChromeDriver()
+
+            } ;
+             
+        }
+    }
+
+    public enum BrowserType
+    {
+        Chrome,
+        Safari,
+        Firefox,
+        EdgeChromiun
+    }
+}
